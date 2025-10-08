ffdfe806339ae12d6ec1185a05d1f393daef91c6
Yujie Deng <ydeng@MacBook-YDE-Rezon.local>
2025-10-08 16:56:43 +0200

    update conditions

--- pages/Jeu_Q1.py
+++ pages/Jeu_Q1.py
@@ -76,60 +76,60 @@ with st.expander("🕒 BILAN KLEMO", expanded=True):
     else:
         st.info("👉 Cliquez sur **LANCER LA SIMULATION KLEMO** pour commencer.")
 
-with st.expander("💡 RECOS KLEMO", expanded=True):
-    if 'json_synth_id' not in st.session_state:
-        st.session_state.json_synth_id = None
+st.subheader("💡 RECOS KLEMO")
+if 'json_synth_id' not in st.session_state:
+    st.session_state.json_synth_id = None
 
-    with st.expander("📝 FORMULAIRE OBJECTIF", expanded=True):
-            # Text area for the next API call
-            # st.subheader("RECOS KLEMO ")
-            # st.text_area("Payload for API Bilan Pat", json.dumps(json_proj, indent=2), height=300, key="bilanpat_content")
-            # st.text_area(":black_nib: Enter Json:", value=json.dumps(st.session_state.json_proj, indent=2),height = 200, key = "bilanpat_content")
-            # payload_strat_txt = st.text_area(":black_nib: Enter JSON payload of Strat Pat:", height=200)
-            
-        col1, col2 = st.columns(2)
+with st.expander("📝 FORMULAIRE OBJECTIF", expanded=True):
+        # Text area for the next API call
+        # st.subheader("RECOS KLEMO ")
+        # st.text_area("Payload for API Bilan Pat", json.dumps(json_proj, indent=2), height=300, key="bilanpat_content")
+        # st.text_area(":black_nib: Enter Json:", value=json.dumps(st.session_state.json_proj, indent=2),height = 200, key = "bilanpat_content")
+        # payload_strat_txt = st.text_area(":black_nib: Enter JSON payload of Strat Pat:", height=200)
+        
+    col1, col2 = st.columns(2)
 
-        with col1:
-            # Objective selection
-            selected_objectif = st.selectbox(
-                "Choisir un Objectif",
-                options=list(func.OBJECTIF_CHOICES.keys()),
-                key="objectif_select"
-            )
-        
-        with col2:
-            # Sub-objective selection (dynamic based on objective)
-            selected_sous_objectif = st.selectbox(
-                "Choisir un Objectif Détaillé",
-                options=func.OBJECTIF_CHOICES[selected_objectif],
-                key="sous_objectif_select"
-            )
-        
-        paramObj = st.text_area(":black_nib: entrer des paramètres d'objectif (optionnel):",value='{"debut":"2029-10-31", "montantRegulier":400,"horizon":24}', height=200)
-        
-    if st.button("LANCER SIMULATION RECOS KLEMO"):
-        # Build the payload
-        st.session_state.payload_strat = {
-            "requestId": st.session_state.json_synth_id, 
-            "requestKey": st.session_state.json_synth_key,
-            "objectif": func.MAPPINGS_OBJECTIF_CHOICES[selected_objectif],
-            "sousObjectif": func.MAPPINGS_OBJECTIF_CHOICES[selected_sous_objectif],
-            "paramObjectif": json.loads(paramObj),
-            "investorProfile":{"level":"Balanced","esg":"Neutral"}
-        }
-
-    # json_proj = json.loads(json_proj)
-    # if st.button("Send Request to our API StratPat"):
-        if st.session_state.json_synth_id: 
-            # Call the API with the parsed JSON
-            init_strat, time_elapsed = func.call_api(st.session_state.payload_strat,func.STRAT_INIT_URL)
+    with col1:
+        # Objective selection
+        selected_objectif = st.selectbox(
+            "Choisir un Objectif",
+            options=list(func.OBJECTIF_CHOICES.keys()),
+            key="objectif_select"
+        )
     
-            if init_strat:
-                request_id     = st.session_state.payload_strat.get("requestId")
-                strat_result   = func.poll_result(func.STRAT_URL,request_id)
-                print("debugging: strat_result",strat_result)
-                strat_output = strat_result.json()["output"]
-                func.display_strat_output(selected_objectif,selected_sous_objectif, st.session_state.payload_strat, strat_output,5)
+    with col2:
+        # Sub-objective selection (dynamic based on objective)
+        selected_sous_objectif = st.selectbox(
+            "Choisir un Objectif Détaillé",
+            options=func.OBJECTIF_CHOICES[selected_objectif],
+            key="sous_objectif_select"
+        )
+    
+    paramObj = st.text_area(":black_nib: entrer des paramètres d'objectif (optionnel):",value='{"debut":"2029-10-31", "montantRegulier":400,"horizon":24}', height=200)
+    
+if st.button("LANCER SIMULATION RECOS KLEMO"):
+    # Build the payload
+    st.session_state.payload_strat = {
+        "requestId": st.session_state.json_synth_id, 
+        "requestKey": st.session_state.json_synth_key,
+        "objectif": func.MAPPINGS_OBJECTIF_CHOICES[selected_objectif],
+        "sousObjectif": func.MAPPINGS_OBJECTIF_CHOICES[selected_sous_objectif],
+        "paramObjectif": json.loads(paramObj),
+        "investorProfile":{"level":"Balanced","esg":"Neutral"}
+    }
+
+# json_proj = json.loads(json_proj)
+# if st.button("Send Request to our API StratPat"):
+    if st.session_state.json_synth_id: 
+        # Call the API with the parsed JSON
+        init_strat, time_elapsed = func.call_api(st.session_state.payload_strat,func.STRAT_INIT_URL)
+
+        if init_strat:
+            request_id     = st.session_state.payload_strat.get("requestId")
+            strat_result   = func.poll_result(func.STRAT_URL,request_id)
+            print("debugging: strat_result",strat_result)
+            strat_output = strat_result.json()["output"]
+            func.display_strat_output(selected_objectif,selected_sous_objectif, st.session_state.payload_strat, strat_output,5)
 
 # streamlit, pre-save a json, show in a pliable section base info: age, situation, emprunt, give possibility to change it and save to state session, then add a button to launch a api call
 
